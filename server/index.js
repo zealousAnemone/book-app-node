86f7da24871e4068b5a0fa30d0bfd129f3dbece6
Veronica Stork <zealous.anemone@gmail.com>
2021-02-13 19:32:19 -0500

    Display book titles and authors

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const pg = require('pg');
+
+const connectionUrl =
+  'postgres://wqvuvscz:mEbbbHx6mlXN7mLlAvr5sBbAcJuTNWvy@ziggy.db.elephantsql.com:5432/wqvuvscz';
+const pool = new pg.Pool({
+  connectionString: connectionUrl,
+});
+
+const showBooks = (req, res, next) => {
+  const text =
+    'SELECT public.books.title, public.authors.name FROM public.books INNER JOIN public.authors ON public.books.author_id = public.authors.author_id';
+  pool.query(text, (err, response) => {
+    if (err) {
+      return next(err);
+    }
+    res.locals.books = response.rows;
+    next();
+  });
+};
+
+const showAuthor = (req, res, next) => {
+  const text = `SELECT public.books.title, public.authors.name FROM public.books INNER JOIN public.authors ON public.books.author_id = public.authors.author_id WHERE public.authors.name = '${req.body.name}'`;
+  pool.query(text, (err, response) => {
+    if (err) {
+      return next(err);
+    }
+    console.log('Response.rows: ', response.rows);
+    res.locals.author = response.rows;
+    next();
+  });
+};
+
+app.use(express.json());
+
+app.get('/books', showBooks, (req, res) =>
+  res.status(200).json(res.locals.books)
+);
+
+app.post('/author', showAuthor, (req, res) => {
+  res.status(200).json(res.locals.author);
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [author, setAuthor] = useState('');
+  let bookList = [];
+
+  const displayAuthor = (name) => {
+    let author = { name };
+    fetch('/author', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(author),
+    }).then((res) => res.json());
+  };
+
+  useEffect(() => {
+    fetch('/books')
+      .then((res) => res.json())
+      .then((books) => {
+        setBooks(books);
+      });
+  }, []);
+
+  for (let i = 0; i < books.length; i++) {
+    bookList.push(
+      <li key={i}>
+        {books[i].title},{' '}
+        <span
+          className="author-name"
+          id={books[i].name}
+          onClick={(e) => {
+            displayAuthor(e.target.id);
+          }}
+        >
+          {books[i].name}
+        </span>
+      </li>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>{bookList}</ul>
     </div>
   );
 }
